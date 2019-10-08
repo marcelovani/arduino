2bc69d377879d70f428a9e8b0fc250abf6a7b46c
Marcelo Vani <marcellovani@yahoo.co.uk>
2019-10-09 00:51:44 +0100

    Cleanup

--- accelerometer/javascript/charts/sma.js
+++ accelerometer/javascript/charts/sma.js
@@ -133,37 +133,3 @@ function setMarkers(val, markerType, markerColor, indexLabel) {
 		}
 	}
 }
-
-function simple_moving_averager(period) {
-    var nums = [];
-    return function(num) {
-        nums.push(num);
-        if (nums.length > period)
-            nums.splice(0,1);  // remove the first element of the array
-        var sum = 0;
-        for (var i in nums)
-            sum += nums[i];
-        var n = period;
-        if (nums.length < period)
-            n = nums.length;
-        return(sum/n);
-    }
-}
-
-function calcSMA2(data, samples) {
-	var sma = simple_moving_averager(samples);
-	var tmp = [];
-	var smaData = [];
-	var smaPoints = [];
-	for (i = 0; i < data.length; i++) {
-		tmp[i] = data[i].y;
-	}
-	for (var i in tmp) {
-	    var n = tmp[i];
-	  	smaPoints.push({ 
-            x: data[i].x,
-            y: parseFloat(sma[n])
-        });
-	}
-	return smaPoints;
-}
