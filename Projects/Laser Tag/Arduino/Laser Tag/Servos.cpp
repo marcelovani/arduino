b16fc52145c00ff852af08569a11fddf6843152c
Marcelo Vani <marcellovani@yahoo.co.uk>
2023-01-23 14:38:16 +0000

    Added timer to servos.

--- Projects/Laser Tag/Arduino/Laser Tag/Servos.cpp
+++ Projects/Laser Tag/Arduino/Laser Tag/Servos.cpp
@@ -1,11 +1,14 @@
 #include <Servo.h>
 
 class Servos: public Runnable {
-    byte pin;
-    Servo servo;
+  byte pin;
+  Servo servo;
 
   private:
+    MillisTimer _timerOn;
     byte position;
+    byte start_position;
+    byte drop_position;
 
   public:
     Servos(byte pin) : pin(pin) {
@@ -14,23 +17,55 @@ class Servos: public Runnable {
     void setup() {
       servo.attach(pin);
       // @todo make this configurable.
-      set(160);
+      this->start_position = 160;
+      this->drop_position = 90;
+      this->reset();
     }
 
     void loop() {
+      this->timerLoop();
     }
 
-    void set(byte position) {
-      this->position = position;
+    void reset() {
+      this->position = this->start_position;
       this->send();
     }
 
     void drop() {
-      this->position = 90;
+      this->position = this->drop_position;
       this->send();
+      // Timer delay between 10 and 30 seconds.
+      this->timerOnStart(10 + rand() % 20);
     }
 
     void send() {
       servo.write(this->position);
+    }
+
+    /**
+     * Used to check if the target is up or dropped.
+     */
+    boolean targetIsOn() {
+      return this->position = this->start_position;
+    }
+
+    /**
+     * Start a timer to reset the servo position.
+     */
+    void timerOnStart(unsigned long _interval)
+    {
+      this->_timerOn.setInterval(_interval * 1000);
+      this->_timerOn.setRepeats(1);
+      this->_timerOn.start();
+    }
+
+    /**
+     * Timer loop controller.
+     */
+    void timerLoop() {
+      this->_timerOn.run();
+      if (!this->_timerOn.isRunning()) {
+        this->reset();
+      }
     }
 };
